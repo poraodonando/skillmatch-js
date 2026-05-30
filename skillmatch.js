a3ee7a7c89afdd508ebff4369ae2b3bc2da80f95
Nando Batista <65638072+poraodonando@users.noreply.github.com>
2026-05-30 18:54:51 -0300

    criando lista de vagas

--- skillmatch.js
+++ skillmatch.js
@@ -11,3 +11,42 @@ const candidato = {
     habilidades: ["JavaScript", "GitHub", "Lógica de Programação", "Godot","Unity5","HTML","CSS"],
     experienciaMeses: 1
 }
+
+//RF02 – Criar uma lista de vagas
+
+
+const lista_vagas = [
+  {
+    id: 1,
+    empresa: "AuraTech",
+    cargo: "Desenvolvedor Front-End Júnior",
+    requisitos: ["JavaScript", "GitHub", "Lógica de Programação"],
+    salario: 8100,
+    modalidade: "Remoto"
+  },
+  {
+    id: 2,
+    empresa: "67Lab",
+    cargo: "Estágio Front-End",
+    requisitos: ["JavaScript", "Kanban", "GitHub"],
+    salario: 1800,
+    modalidade: "Híbrido"
+  },
+
+  {
+    id: 3,
+    empresa: "AlfaSolutions",
+    cargo: "Programador JS Júnior",
+    requisitos: ["JavaScript", "Arrays", "Objetos", "Funções", "HTML", "CSS"],
+    salario: 6300,
+    modalidade: "Presencial"  
+  },
+  {
+    id: 4,
+    empresa: "BetaCore",
+    cargo: "Front-End Pleno",
+    salario:9600,
+    requisitos:["HTML","CSS","TypeScript","React","Banco de dados","OOP","Kanban"],
+    modalidade:"Remoto"
+  }
+];
