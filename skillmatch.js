9ccbaba7cd6b4af2d582508f274eecdcec1384e4
Nando Batista <65638072+poraodonando@users.noreply.github.com>
2026-05-30 18:06:22 -0300

    adicionando candidato

---
+++ skillmatch.js
@@ -0,0 +1,13 @@
+console.log("Bem vindo ao Projeto Skill Match")
+
+
+
+
+// RF01 - Perfil do candidato
+
+const candidato = {
+    nome: 'Nando',
+    area: 'Front-End',
+    habilidades: ["JavaScript", "GitHub", "Lógica de Programação", "Godot","Unity5","HTML","CSS"],
+    experienciaMeses: 1
+}
